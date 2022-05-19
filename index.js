6c5f16173e041a07ed34633fefe7515bb0208912
nur-ahmed <nurnknklolo7333@gmail.com>
2022-05-19 16:31:31 +0300

    creat api

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const listRoute = require("./routes/lists");
+const movieRoute = require("./routes/movies");
+const cors = require("cors");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+   
+  })
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.error(err);
+  });
+app.use(cors({
+  origin: ["http://localhost:4000", "http://localhost:3000"]
+}));
+app.use(express.json());
+
+app.use("/api/movies", movieRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/lists", listRoute);
+
+
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
